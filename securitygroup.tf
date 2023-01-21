d63e3fea384fc2941d3d55692b0fb64e6b0bb3b9
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-21 12:23:24 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,25 +1,28 @@
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "allow ssh and http traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  required_version = ">= 1.2.0"
+}
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+provider "aws" {
+  region  = "ap-south-1"
+  access_key = "AKIASDFMEMBBPMOGKNHL"
+
+  secret_key = "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"
+}
+
+resource "aws_instance" "terraform_instance" {
+  ami = "ami-0763cf792771fe1bd"
+  instance_type = "t2.micro"
+  key_name = "krishnakeypair"
+  security_groups = ["krishnasecuritygroup"]
+  tags = {
+     Name = "terraform_instance"
   }
 }
+
