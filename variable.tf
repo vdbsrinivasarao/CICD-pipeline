3542bb862e41ba6a1a46b5c1797b74262e0c0e3f
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-21 12:53:14 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIASDFMEMBBPMOGKNHL"
-  }
-variable "secret_key" {
-  default = "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"
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
