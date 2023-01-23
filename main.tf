5b0fc000819a5b2a9fb9e1f207fb74b6b254dfb9
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-23 16:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,25 +13,7 @@ terraform {
 provider "aws" {
   region  = "ap-south-1"
 }
-ingress {
-  from_port  =8080
-  to_port    =8080
-  protocol   ="tcp"
-  cidr_blocks =["0.0.0.0/0"]
-}
-ingress {
-  from_port  = 22
-  to_port    =22
-  protocol   ="tcp"
-  cidr_blocks =["0.0.0.0/0"]
-}
 
-egress {
-  from_port  = 0
-  to_port    =22
-  protocol   ="tcp"
-  cidr_blocks =["0.0.0.0/0"]
-}
 resource "aws_instance" "example" {
   ami           = "ami-0763cf792771fe1bd"
   instance_type = "t2.micro"
