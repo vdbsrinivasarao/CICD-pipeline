e575d1f5ba6e74b024b9e66311411957c842bac4
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-23 14:45:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,18 +5,13 @@ terraform {
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
-
 provider "aws" {
   region  = "ap-south-1"
   access_key = "AKIASDFMEMBBCRK7O67W "
-
   secret_key = "gq3mghYJ/iVGqdgGiP7cjPBxGl0QYqD5G9DVmFth "
-  
 }
-
 resource "aws_instance" "example" {
   ami           = "ami-0763cf792771fe1bd"
   instance_type = "t2.micro"
