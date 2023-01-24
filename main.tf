2a4c63c5f076e627b61ff09343368d3d6bf8fd08
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-24 17:16:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIASDFMEMBBF52B43SI"
-  secret_key = "SisOq/xGz9PF8ftZ4gyEuIKUXJ2k6jz4Ej6/AQkS "
-
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0763cf792771fe1bd"
+  count=1
   instance_type = "t2.micro"
   key_name = "krishnakeypair"
   security_groups = ["krishnasecuritygroup"]
