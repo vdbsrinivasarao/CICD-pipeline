a090a751b97304839e93bbcf7cd1d4f5cdfdf3ba
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-25 13:19:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0763cf792771fe1bd"
   count=1
   instance_type = "t2.micro"
-  key_name = "krishnakeypair"
-  security_groups = ["krishnasecuritygroup"]
+  key_name = "brahmajikeypair"
+  security_groups = ["brahmajisg"]
   tags = {
     Name = "Terraform ServerInstance"
   }
